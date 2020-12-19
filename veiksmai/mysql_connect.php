56bb8db287b0bfefe72ce774158291bdb51a9ef3
jonasss312 <jonasss312@gmail.com>
2020-12-19 18:42:41 +0200

    mysl connect

--- veiksmai/mysql_connect.php
+++ veiksmai/mysql_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'sporto_klubas');
-DEFINE ('DB_USER', 'root');
-DEFINE ('DB_PASSWORD', '');
+DEFINE ('DB_HOST', 'freedb.tech');
+DEFINE ('DB_NAME', 'freedbtech_Sportas');
+DEFINE ('DB_USER', 'freedbtech_Sportas');
+DEFINE ('DB_PASSWORD', 'sportas123');
 
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die('Nepavyko prisijungti prie DB' .
