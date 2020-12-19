18cfad20715489aa8d415291314d53c21619c956
IgnasKrivas <ignaskriv@gmail.com>
2020-12-19 20:00:29 +0200

    bootstrap riperoni

--- prenumeratos.php
+++ prenumeratos.php
@@ -11,9 +11,10 @@ $result = mysqli_query($dbc,$sql);
 <head>
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <title>GYM</title>
 </head>
 <body>
@@ -42,9 +43,9 @@ $result = mysqli_query($dbc,$sql);
 	{	 
 		$pavadinimas=$row['pavadinimas']; 
 		?>
-            <div class="card" style="width: 18rem;">
+            <div class="card border-dark" style="width: 18rem;">
 			<?php
-			echo "<h5 class=\"card-title\">".$pavadinimas."</h5>";    
+			echo "<h5 class=\"card-title\">$pavadinimas</h5>";    
 			?>
 			<div class="card-body">
 		<?php
