301be5e019fb8b802b2ff83d7bc3112b0e532843
jonasss312 <jonasss312@gmail.com>
2020-12-20 20:13:34 +0200

    123

--- profilioPrenumeratos.php
+++ profilioPrenumeratos.php
@@ -1,3 +1,6 @@
+<?php
+include('./include/navbar.php');
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,27 +13,6 @@
 </head>
 <body>
 <div class="container-fixed">
-      
-      <div class="navbar navbar-default">
-        <div class="container">
-          <a class="navbar-brand" href="#">BELEKOKS GYMAS</a>
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="pagrindinis.php">Pagrindinis</a></li>
-            <li><a href="prenumeratos.php">Prenumeratos</a></li>
-            <li><a href="paslaugos.php">Paslaugos</a></li>
-            <li class="divider-vertical"></li>
-			<li><a href="ivykiai.php">Ivykiai</a></li>
-            <li><a href="tvarkarastis.php">Tvarkaraštis</a></li>
-            <li><a href="profilis.php">Profilis</a></li>
-			<li><a href="administratorius.php">Administratorius</a></li>
-			<li><a>Login: <input type="text" placeholder="" size=4</input></li></a>
-			<li><a>Pass: <input type="text" placeholder="" size=4</input></li></a>
-			<li><a href="pagrindinisPrisijunges.php" type="button2" class="btn"><button>Prisijungti</button></a></li>
-          </ul>
-        </div>
-      </div>
-	  
-
       <div class="jumbotron text-center">
         <h1>Prenumeratos</h1>
 	  </div>
@@ -38,8 +20,11 @@
 	<div class="container">
        <div>
 			<h2>Užsakytos prenumeratos</h2>
-			<p> prenumerata 1 <button type="button2" onclick=trynimas() class="btn btn-primary">Atsisakyti</button></p>
-		</div>
+      <?php
+      
+      
+      ?>
+      </div>
 		<script>
 		function trynimas() {
     alert("Pavyko sėkmingai");
